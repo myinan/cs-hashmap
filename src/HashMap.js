920a6c4c37dce575aa355303dd5147ecabc4efc3
myinan <m.yasir.inan@gmail.com>
2024-01-21 01:30:46 +0300

    create get(key) method

--- src/HashMap.js
+++ src/HashMap.js
@@ -6,7 +6,7 @@ export default class HashMap {
     this.tableLength = 16;
   }
 
-  static hash(string) {
+  static #hash(string) {
     let hashCode = 0;
 
     const primeNumber = 31;
@@ -23,26 +23,48 @@ export default class HashMap {
       value,
     };
 
-    const index = HashMap.hash(key) % this.tableLength;
+    const index = HashMap.#hash(key) % this.tableLength;
     if (this.table[index]) {
-      this.table[index].append(obj);
+      if (!HashMap.#replaceKey(this.table[index], obj)) {
+        this.table[index].append(obj);
+      }
     } else if (!this.table[index]) {
-      this.table[index] = new LinkedList(obj);
+      this.table[index] = new LinkedList();
+      this.table[index].append(obj);
     }
   }
 
+  static #replaceKey(list, obj) {
+    let cur = list.head;
+    while (cur) {
+      if (cur.value.key === obj.key) {
+        cur.value.value = obj.value;
+        return true;
+      }
+      cur = cur.next;
+    }
+    return false;
+  }
+
   get(key) {
-    const index = HashMap.hash(key) % this.tableLength;
-    return this.table[index];
+    const index = HashMap.#hash(key) % this.tableLength;
+    let cur = this.table[index].head;
+
+    while (cur) {
+      if (cur.value.key) {
+        return cur.value.value;
+      }
+      cur = cur.next;
+    }
+    return null;
   }
 }
 
 const hashTable = new HashMap();
 hashTable.set("testKey", 5);
 hashTable.set("testKey", 6);
+hashTable.set("anotherTestt", "tested");
+hashTable.set("anotherTest", "1233");
 console.log(hashTable);
 
-console.log(HashMap.hash("testKey") % 16);
-
 console.log(hashTable.get("testKey"));
-console.log(hashTable.get("testKey").size);
